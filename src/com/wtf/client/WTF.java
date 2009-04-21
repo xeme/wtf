01d506f130d55a04e13d70fe5325633a3693db5b
Filip Balejko <filip.balejko@gmail.com>
2009-04-21 20:37:56 +0200

    xhtml fixed

--- src/com/wtf/client/WTF.java
+++ src/com/wtf/client/WTF.java
@@ -17,6 +17,9 @@ import com.google.gwt.user.client.ui.RootPanel;
  */
 public class WTF implements EntryPoint {
 
+	private com.google.gwt.user.client.Element _selected = null;
+	
+	
   /**
    * This is the entry point method.
    */
@@ -24,31 +27,33 @@ public class WTF implements EntryPoint {
     HTML status = new HTML();
     status.setText("tutaj status wtf..");
 
- //   RootPanel.get("wtf_status").add(status);  
+    RootPanel.get("wtf_status").add(status);  
     initDOM();  
     
   }
 
   
   public void elemOver(com.google.gwt.user.client.Element elem){
-	  if(DOM.getElementById("wtf_selection") != null | elem == RootPanel.getBodyElement()) {
-//		  GWT.log("elemOver return" + elem.toString(), null);
+	  if(DOM.getElementById("wtf_selection") != null || elem == RootPanel.getBodyElement()
+			  || elem.equals(_selected)) {
+		  GWT.log("elemOver [return]" + elem.toString(), null);
 		  return;
 	  }
-//	  GWT.log("elemOver" + elem.toString(), null);	
+	  GWT.log("elemOver" + elem.toString(), null);	
 	  
 	  Element div = DOM.createDiv();
 	  div.setId("wtf_selection");
-	  elem.appendChild(div);
+	  RootPanel.getBodyElement().appendChild(div);
+	  _selected = elem;
 	  
 
 	  com.google.gwt.user.client.Element div_ = (com.google.gwt.user.client.Element) div;
 	  DOM.setStyleAttribute(div_, "border", "solid 2px #3c7bd9");
-	  DOM.setStyleAttribute(div_, "height", Integer.toString(elem.getClientHeight()));
-	  DOM.setStyleAttribute(div_, "width", Integer.toString(elem.getClientWidth()));
+	  DOM.setStyleAttribute(div_, "height", Integer.toString(elem.getClientHeight())+"px");
+	  DOM.setStyleAttribute(div_, "width", Integer.toString(elem.getClientWidth())+"px");
 	  DOM.setStyleAttribute(div_, "position", "absolute");
-	  DOM.setStyleAttribute(div_, "top", Integer.toString(elem.getAbsoluteTop()));
-	  DOM.setStyleAttribute(div_, "left", Integer.toString(elem.getAbsoluteLeft()));
+	  DOM.setStyleAttribute(div_, "top", Integer.toString(elem.getAbsoluteTop())+"px");
+	  DOM.setStyleAttribute(div_, "left", Integer.toString(elem.getAbsoluteLeft())+"px");
 	
 	  DOM.sinkEvents(div_, Event.ONMOUSEOUT);
 	  DOM.setEventListener(div_, new EventListener() {
@@ -56,7 +61,8 @@ public class WTF implements EntryPoint {
 			  com.google.gwt.user.client.Element elem = DOM.eventGetTarget(event);
 			  switch (DOM.eventGetType(event)) {
 		      case Event.ONMOUSEOUT:
-		    	  elem.getParentElement().removeChild(elem);
+		    	  RootPanel.getBodyElement().removeChild(elem);
+		    	  GWT.log("AutoDestruction" + elem.toString(), null);
 		        break;
 		    }
 		  }
@@ -65,25 +71,31 @@ public class WTF implements EntryPoint {
   
   public void selectionClean(com.google.gwt.user.client.Element elem){
 		  com.google.gwt.user.client.Element sel = DOM.getElementById("wtf_selection");
-		  if(sel != null && !DOM.isOrHasChild(elem, sel))
-			  sel.getParentElement().removeChild(sel);
+		  if(sel != null && !elem.equals(_selected) && !sel.equals(elem))
+		  {
+			  GWT.log("selectionClean" + elem.toString(), null);
+			  RootPanel.getBodyElement().removeChild(sel);
+		  }
   }
   
   private void elemInit(com.google.gwt.user.client.Element elem){
-//	  DOM.setStyleAttribute(elem, "border", "solid 1px #0f0"); //troche niefajnie
+//	  DOM.setStyleAttribute(elem, "backgroundColor", "#0f0"); //troche niefajnie
   }
   
   private void addListener(com.google.gwt.user.client.Element elem){
 	  DOM.sinkEvents(elem, Event.ONMOUSEOVER | Event.ONMOUSEOUT);
+	  GWT.log("**ADD_LISTENER" + elem.toString() + "id:" + elem.getId(), null);
 	  DOM.setEventListener(elem, new EventListener() {
 		  public void onBrowserEvent(Event event) {
 			  com.google.gwt.user.client.Element elem = DOM.eventGetTarget(event);
 			  switch (DOM.eventGetType(event)) {
 		      case Event.ONMOUSEOVER:
+		    	  GWT.log("_over_ " + elem.toString(), null);
 		    	  selectionClean(elem);
 		    	  elemOver(elem);
 		        break;
 		      case Event.ONMOUSEOUT:
+		    	  GWT.log("_out_ " + elem.toString(), null);
 		    	  selectionClean(elem);
 		        break;
 		    }
